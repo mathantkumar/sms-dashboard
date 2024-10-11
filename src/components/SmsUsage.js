ab986ecc9b304ab98b0c7944386557ff38e41620
mathantkumar <mathankumar2001@gmail.com>
2024-10-11 19:26:08 +0530

    minor change

--- src/components/SmsUsage.js
+++ src/components/SmsUsage.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const SmsUsage = ({ smsCountLastMinute, totalSmsToday }) => {
   const gaugeData = {
