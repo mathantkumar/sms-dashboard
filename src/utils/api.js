7b6834433b6d28dc7da469450f25e6618657bc45
mathantkumar <mathankumar2001@gmail.com>
2024-10-12 00:37:39 +0530

    minor change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://smsense.vercel.app";
+//const API_BASE_URL = "https://smsense.vercel.app";
 
 export const fetchStatistics = async () => {
-  const response = await axios.get(`${API_BASE_URL}/stats`);
+  const response = await axios.get(`/stats`);
   return response.data;
 };
 
 export const fetchRateLimitViolations = async () => {
-  const response = await axios.get(`${API_BASE_URL}/violations`);
+  const response = await axios.get(`/violations`);
   return response.data;
 };
 
 export const sendSms = async (phone) => {
-  const response = await axios.post(`${API_BASE_URL}/send-sms`, { phone });
+  const response = await axios.post(`/send-sms`, { phone });
   return response.data;
 };
